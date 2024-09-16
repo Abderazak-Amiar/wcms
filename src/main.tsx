c512209feef7cf508b1ce642e218933deefd838c
Abderazak-Amiar <amiar.abderazak@gmail.com>
2024-09-16 08:21:13 +0100

    feat: integrate ApolloProvider & browserRouter on the main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './api/apollo.ts';
+import App from './App.tsx';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
-)
+);
 
 // Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+// window.ipcRenderer.on('main-process-message', (_event, message) => {
+//   console.log(message)
+// })
